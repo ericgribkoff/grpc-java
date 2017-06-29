1d22fcf7ee69ee7ae659407f0e96dba5ad0d6750
Eric Gribkoff <ericgribkoff@google.com>
2017-06-29 13:55:38 -0700

    add docker rmi, testing options set up

--- android-interop-testing/run-instrumented-tests-on-firebase.sh
+++ android-interop-testing/run-instrumented-tests-on-firebase.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+
+# Builds the gRPC Android instrumented interop tests inside a docker container
+# and runs them on Firebase Test Lab
+
+DOCKER_IMAGE=android_interop_test
+SERVICE_KEY=~/android-interops-service-key.json
+
 git clone --branch android_interop_docker https://github.com/ericgribkoff/grpc-java.git
 cd grpc-java/docker/android-interop-testing
-docker build -t android_interop_test .
+docker build -t $DOCKER_IMAGE .
 
-export SERVICE_KEY=~/android-interopsServiceAccountKey.json
 docker run --interactive --rm \
   --volume=$SERVICE_KEY:/serviceAccountKey.json:ro \
-  android_interop_test \
+  $DOCKER_IMAGE \
       /bin/bash -c "gcloud auth activate-service-account --key-file=/serviceAccountKey.json; \
-      gcloud config set project grpc-testing; \
-      gcloud firebase test android run \
-        --type instrumentation \
-        --app /grpc-java/android-interop-testing/app/build/outputs/apk/app-debug.apk \
-        --test /grpc-java/android-interop-testing/app/build/outputs/apk/app-debug-androidTest.apk \
-        --device model=Nexus6,version=21,locale=en,orientation=portrait"
+      gcloud config set project grpc-testing; exit 1"
+#      gcloud firebase test android run \
+#        --type instrumentation \
+#        --app /grpc-java/android-interop-testing/app/build/outputs/apk/app-debug.apk \
+#        --test /grpc-java/android-interop-testing/app/build/outputs/apk/app-debug-androidTest.apk \
+#        --device model=Nexus6,version=21,locale=en,orientation=portrait"
+TEST_STATUS=$?
+docker rmi $DOCKER_IMAGE
+exit $TEST_STATUS
