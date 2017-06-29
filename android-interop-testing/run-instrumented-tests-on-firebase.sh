afd644b41ba264f020fb527ab92db2137ec43c93
Eric Gribkoff <ericgribkoff@google.com>
2017-06-29 12:48:23 -0700

    fix multiple commands to docker run

--- android-interop-testing/run-instrumented-tests-on-firebase.sh
+++ android-interop-testing/run-instrumented-tests-on-firebase.sh
@@ -7,10 +7,10 @@ export SERVICE_KEY=~/android-interopsServiceAccountKey.json
 docker run --interactive \
   --volume=$SERVICE_KEY:/serviceAccountKey.json:ro \
   android_interop_test \
-      gcloud auth activate-service-account --key-file="/serviceAccountKey.json" && \
-      gcloud config set project grpc-testing && \
+      /bin/bash -c "gcloud auth activate-service-account --key-file=/serviceAccountKey.json; \
+      gcloud config set project grpc-testing; \
       gcloud firebase test android run \
         --type instrumentation \
         --app /grpc-java/android-interop-testing/app/build/outputs/apk/app-debug.apk \
         --test /grpc-java/android-interop-testing/app/build/outputs/apk/app-debug-androidTest.apk \
-        --device model=Nexus6,version=21,locale=en,orientation=portrait
+        --device model=Nexus6,version=21,locale=en,orientation=portrait"
