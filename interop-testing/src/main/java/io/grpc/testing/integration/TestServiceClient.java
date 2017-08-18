3b5d39cc9abe194b218fb6ba4c935f7f9b621313
Eric Gribkoff <ericgribkoff@google.com>
2017-08-18 00:13:12 -0700

    diagnosing swallowed netty deframe failed exception

--- interop-testing/src/main/java/io/grpc/testing/integration/TestServiceClient.java
+++ interop-testing/src/main/java/io/grpc/testing/integration/TestServiceClient.java
@@ -330,11 +330,11 @@ public class TestServiceClient {
           builder.overrideAuthority(serverHostOverride);
         }
         // TODO without this and large unary test (with size * 100) Netty fails silently.
-        builder.maxInboundMessageSize(31415911);
+//        builder.maxInboundMessageSize(31415911);
         return builder.build();
       } else {
         OkHttpChannelBuilder builder = OkHttpChannelBuilder.forAddress(serverHost, serverPort);
-        builder.maxInboundMessageSize(31415911);
+//        builder.maxInboundMessageSize(31415911);
         if (serverHostOverride != null) {
           // Force the hostname to match the cert the server uses.
           builder.overrideAuthority(
