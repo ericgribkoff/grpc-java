bcfaf42e55d16bbb683e38a8840f1992f8471501
Eric Gribkoff <ericgribkoff@google.com>
2017-08-19 10:48:56 -0700

    Moved fix into custom ConnectionSpec passed in when building channel in HelloWorldActivity.
    TLSv1.2 (and TLSv1.1) are disabled by default on Android API levels
    16-19. To use them, you must give OkHttpChannelBuilder a custom
    SSLSocketFactory that explicitly turns these on, otherwise OkHttp's
    io.grpc.okhttp.internal.ConnectionSpec#supportedSpec (which is used
    before apply'ing a spec) will take the intersection of the protocols
    (and ciphers) in the spec with those available on the socket, which will
    not include TLSv1.1 or TLSv1.2 and you end up making an outgoing call
    with no advertised protocol.

--- examples/android/helloworld/app/src/main/java/io/grpc/helloworldexample/HelloworldActivity.java
+++ examples/android/helloworld/app/src/main/java/io/grpc/helloworldexample/HelloworldActivity.java
@@ -28,20 +28,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.squareup.okhttp.CipherSuite;
+import com.squareup.okhttp.ConnectionSpec;
+
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.okhttp.OkHttpChannelBuilder;
 import io.grpc.examples.helloworld.GreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
 
-import org.conscrypt.Conscrypt;
-
-import java.security.Provider;
-import java.security.Security;
+//import org.conscrypt.Conscrypt;
+//
+//import java.security.Provider;
+//import java.security.Security;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.security.KeyManagementException;
 import java.util.concurrent.TimeUnit;
+
+import com.squareup.okhttp.ConnectionSpec;
+import com.squareup.okhttp.TlsVersion;
+
+import javax.net.ssl.SSLSocketFactory;
+
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+
+import static com.squareup.okhttp.ConnectionSpec.MODERN_TLS;
 
 public class HelloworldActivity extends AppCompatActivity {
     private Button mSendButton;
@@ -76,6 +99,64 @@ public class HelloworldActivity extends AppCompatActivity {
         new GrpcTask().execute();
     }
 
+    public class TLSSocketFactory extends SSLSocketFactory {
+
+        private SSLSocketFactory internalSSLSocketFactory;
+
+        public TLSSocketFactory() throws KeyManagementException, NoSuchAlgorithmException {
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, null, null);
+            internalSSLSocketFactory = context.getSocketFactory();
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return internalSSLSocketFactory.getDefaultCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return internalSSLSocketFactory.getSupportedCipherSuites();
+        }
+
+        @Override
+        public Socket createSocket() throws IOException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket());
+        }
+
+        @Override
+        public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(s, host, port, autoClose));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException, UnknownHostException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port, localHost, localPort));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress host, int port) throws IOException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(address, port, localAddress, localPort));
+        }
+
+        private Socket enableTLSOnSocket(Socket socket) {
+            if(socket != null && (socket instanceof SSLSocket)) {
+                ((SSLSocket)socket).setEnabledProtocols(new String[] {"TLSv1.1", "TLSv1.2"});
+            }
+            return socket;
+        }
+    }
+
     private class GrpcTask extends AsyncTask<Void, Void, String> {
         private String mHost;
         private String mMessage;
@@ -94,8 +175,51 @@ public class HelloworldActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... nothing) {
             try {
-                mChannel = ManagedChannelBuilder.forAddress(mHost, mPort)
+                CipherSuite[] APPROVED_CIPHER_SUITES = new CipherSuite[] {
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
+
+                        // Note that the following cipher suites are all on HTTP/2's bad cipher suites list. We'll
+                        // continue to include them until better suites are commonly available. For example, none
+                        // of the better cipher suites listed above shipped with Android 4.4 or Java 7.
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_CBC_SHA,
+                        CipherSuite.TLS_DHE_DSS_WITH_AES_128_CBC_SHA,
+                        CipherSuite.TLS_DHE_RSA_WITH_AES_256_CBC_SHA,
+                        CipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+                        CipherSuite.TLS_RSA_WITH_AES_256_CBC_SHA,
+                        CipherSuite.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+                };
+//                ConnectionSpec spec = new ConnectionSpec.Builder(OkHttpChannelBuilder.DEFAULT_CONNECTION_SPEC)
+//                        .cipherSuites(CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA).build();
+                mChannel = OkHttpChannelBuilder.forAddress(mHost, mPort)
+                        .connectionSpec(
+                                (new ConnectionSpec.Builder(OkHttpChannelBuilder.DEFAULT_CONNECTION_SPEC)
+//                                .cipherSuites(
+//                                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+//                                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+//                                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+//                                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+//                                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_CBC_SHA,
+//                                        CipherSuite.TLS_DHE_DSS_WITH_AES_128_CBC_SHA,
+//                                        CipherSuite.TLS_DHE_RSA_WITH_AES_256_CBC_SHA,
+//                                        CipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256,
+//                                        CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+//                                        CipherSuite.TLS_RSA_WITH_AES_256_CBC_SHA,
+//                                        CipherSuite.TLS_RSA_WITH_3DES_EDE_CBC_SHA
+//                                )
+                                        .cipherSuites(APPROVED_CIPHER_SUITES)
+//                                        .tlsVersions(TlsVersion.TLS_1_1)
+                                        .tlsVersions(TlsVersion.TLS_1_2) //, TlsVersion.TLS_1_1) //, TlsVersion.TLS_1_0)
+//                                        .supportsTlsExtensions(true)
+                                        .build()))
 //                    .usePlaintext(true)
+                        .sslSocketFactory(new TLSSocketFactory())
                     .build();
                 GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(mChannel);
                 HelloRequest message = HelloRequest.newBuilder().setName(mMessage).build();
