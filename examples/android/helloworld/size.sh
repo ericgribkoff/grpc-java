52253410d3d7de93de1ee3b6c0a99917334c77ad
Eric Gribkoff <ericgribkoff@google.com>
2018-01-17 23:40:52 -0800

    variable names

--- examples/android/helloworld/size.sh
+++ examples/android/helloworld/size.sh
@@ -11,27 +11,27 @@ cd grpc-java/
 #./gradlew install
 cd examples/android/helloworld/
 ./gradlew build
-IFS=, read -r postBytes postKilobytes postMegabytes < <(sed -n 2p app/build/outputs/apksize/release/release.csv)
-IFS=, read -r postMethods postFields postClasses < <(sed -n 2p app/build/outputs/dexcount/release.csv)
+IFS=, read -r new_bytes new_kb new_mb < <(sed -n 2p app/build/outputs/apksize/release/release.csv)
+IFS=, read -r new_methods new_fields new_classes < <(sed -n 2p app/build/outputs/dexcount/release.csv)
 
 cd ../../../
 git checkout examples_with_minify_apk_size
 #./gradlew install
 cd examples/android/helloworld/
 ./gradlew build
-IFS=, read -r preBytes preKilobytes preMegabytes < <(sed -n 2p app/build/outputs/apksize/release/release.csv)
-IFS=, read -r preMethods preFields preClasses < <(sed -n 2p app/build/outputs/dexcount/release.csv)
+IFS=, read -r old_bytes old_kb old_mb < <(sed -n 2p app/build/outputs/apksize/release/release.csv)
+IFS=, read -r old_methods old_fields old_classes < <(sed -n 2p app/build/outputs/dexcount/release.csv)
 
-bytesDelta=$((postBytes-preBytes))
-methodsDelta=$((postMethods-preMethods))
-fieldsDelta=$((postFields-preFields))
+bytesDelta=$((new_bytes-old_bytes))
+methodsDelta=$((new_methods-old_methods))
+fieldsDelta=$((new_fields-old_fields))
 
-echo "Original release APK size in bytes: $preBytes"
-echo "Original release method count: $preMethods"
-echo "Original release field count: $preFields"
-echo "New release APK size in bytes: $postBytes (delta: $bytesDelta)"
-echo "New release method count: $postMethods (delta: $methodsDelta)"
-echo "New release field count: $postFields (delta: $fieldsDelta)"
+echo "Original release APK size in bytes: $old_bytes"
+echo "Original release method count: $old_methods"
+echo "Original release field count: $old_fields"
+echo "New release APK size in bytes: $new_bytes (delta: $bytesDelta)"
+echo "New release method count: $new_methods (delta: $methodsDelta)"
+echo "New release field count: $new_fields (delta: $fieldsDelta)"
 
 # Kororo URLs are in the form "grpc/job/macos/job/master/job/grpc_build_artifacts"
 KOKORO_JOB_PATH=$(echo "${KOKORO_JOB_NAME}" | sed "s|/|/job/|g")
@@ -42,10 +42,9 @@ echo '<html><head></head><body>' > reports/kokoro_index.html
 echo '<h1>'${KOKORO_JOB_NAME}', build '#${KOKORO_BUILD_NUMBER}'</h1>' >> reports/kokoro_index.html
 echo '<h2><a href="https://kokoro2.corp.google.com/job/'${KOKORO_JOB_PATH}'/'${KOKORO_BUILD_NUMBER}'/">Kokoro build dashboard (internal only)</a></h2>' >> reports/kokoro_index.html
 echo '<h2><a href="https://sponge.corp.google.com/invocation?id='${KOKORO_BUILD_ID}'&searchFor=">Test result dashboard (internal only)</a></h2>' >> reports/kokoro_index.html
-echo "<p>New Helloworld release APK size in bytes: $postBytes (delta: $bytesDelta)</p>" >> reports/kokoro_index.html
-echo "<p>New Helloworld release method count: $postMethods (delta: $methodsDelta)</p>" >> reports/kokoro_index.html
-echo "<p>New Helloworld release field count: $postFields (delta: $fieldsDelta)</p>" >> reports/kokoro_index.html
+echo "<p>New Helloworld release APK size in bytes: $new_bytes (delta: $bytesDelta)</p>" >> reports/kokoro_index.html
+echo "<p>New Helloworld release method count: $new_methods (delta: $methodsDelta)</p>" >> reports/kokoro_index.html
+echo "<p>New Helloworld release field count: $new_fields (delta: $fieldsDelta)</p>" >> reports/kokoro_index.html
 echo '</body></html>' >> reports/kokoro_index.html
 
 echo 'Created reports/kokoro_index.html report index'
-
