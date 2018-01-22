3f32c715d78eb2cfcfec94a5cdba30d0eac4862a
Eric Gribkoff <ericgribkoff@google.com>
2018-01-22 09:13:00 -0800

    builds and runs - no protoc yet, but can create channel and log to logcat with gpr_log

--- examples/android/MyCppApp/app/src/main/cpp/native-lib.cpp
+++ examples/android/MyCppApp/app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,11 @@
 #include <jni.h>
 #include <string>
+#include <grpc++/grpc++.h>
+
+//#include "helloworld.grpc.pb.h"
+
+#include <grpc/support/log.h>
+
 
 extern "C"
 JNIEXPORT jstring
@@ -9,5 +15,8 @@ Java_com_example_ericgribkoff_mycppapp_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    grpc::CreateChannel(
+              "localhost:50051", grpc::InsecureChannelCredentials());
+    gpr_log(GPR_ERROR, "yo grpc");
     return env->NewStringUTF(hello.c_str());
 }
