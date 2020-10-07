e3b9cbad0dfdf2917d99dd1676de9ee0173bbe63
Eric Gribkoff <ericgribkoff@google.com>
2020-10-06 23:57:01 -0700

    stats interceptor

--- census-explicit/src/main/java/io/grpc/census/Interceptors.java
+++ census-explicit/src/main/java/io/grpc/census/Interceptors.java
@@ -1,18 +1,39 @@
 package io.grpc.census;
 
+import com.google.common.base.Stopwatch;
+import com.google.common.base.Supplier;
 import io.grpc.ClientInterceptor;
 import io.opencensus.trace.Tracing;
 
 public class Interceptors {
+  private static final Supplier<Stopwatch> STOPWATCH_SUPPLIER = new Supplier<Stopwatch>() {
+    @Override
+    public Stopwatch get() {
+      return Stopwatch.createUnstarted();
+    }
+  };
+
   private Interceptors() {
   }
 
-  public static ClientInterceptor getClientInterceptor() {
+  public static ClientInterceptor getTracingClientInterceptor() {
     CensusTracingModule censusTracing =
             new CensusTracingModule(
                     Tracing.getTracer(),
                     Tracing.getPropagationComponent().getTraceContextFormat());
     return censusTracing.getClientInterceptor();
+  }
+
+
+  public static ClientInterceptor getStatsClientInterceptor() {
+    CensusStatsModule censusStats =
+            new CensusStatsModule(
+                    STOPWATCH_SUPPLIER,
+                    true, /* propagateTags */
+                    true,
+                    true,
+                    true);
+    return censusStats.getClientInterceptor();
   }
 
 }
